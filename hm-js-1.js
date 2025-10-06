0b3ae962b8288bd06f2317c51ae1489bba76ce5b
Anastasiya Shugai <nshugay1@gmail.com>
2025-10-06 13:59:37 +0300

    feat: resolve shortcomings

--- hm-js-1.js
+++ hm-js-1.js
@@ -2,25 +2,22 @@
 
 1. Определи, является ли число чётным/нечётным.`
 
-const checkEven = (num) => {
-    return num % 2 === 0;   
+const isEven = (num) => {
+    if (typeof num !== 'number' || isNaN(num)) {
+        return false;
+    };
+
+    return num % 2 === 0;
 };
 
-let num = 1;
-console.log(checkEven(num));
-
-num = 14;
-console.log(checkEven(num));
-
+//console.log(isEven(4));
+//console.log(isEven(5));
 
 `2. Найди наибольшее из трёх чисел.`
 
-const checkMax = (a, b, c) => {
-    return(Math.max(a, b, c));
-};
+const getMax = (...values) => Math.max(...values);
 
-const maxNumber = checkMax(0, 100, 4);
-console.log(maxNumber);
+//console.log(getMax(0, 100, 4));
 
 
 `3. Напиши калькулятор: две переменные и операция (+, -, *, /), выведи результат.`
@@ -28,10 +25,9 @@ console.log(maxNumber);
 const operators = ['+', '-', '*', '/'];
 
 const calculator = (a, b, operator) => {
-    if (
-        !operators.includes(operator) || typeof a !== 'number' || typeof b !== 'number'
-    ) {
-        return undefined;
+    // проверка на числа
+    if (typeof a !== 'number' || typeof b !== 'number' || isNaN(a) || isNaN(b)) {
+        throw new Error('некорректные переменные');
     };
 
     switch (operator) {
@@ -42,36 +38,31 @@ const calculator = (a, b, operator) => {
         case '*':
             return a * b;
         case '/':
-            return b !== 0 ? a / b : 'error';
+            if (b === 0) throw new Error('деление на ноль');
+            return a / b;
         default:
-            return undefined;
-    }
+            throw new Error('некорректный оператор');
+    };
 };
 
-let a = 2;
-let b = 3;
-let operator = '+';
-console.log(calculator(a, b, operator));
-
-a = 5;
-b = 0;
-operator = '/';
-console.log(calculator(a, b, operator));
+//console.log(calculator(5, 0, '/'));
 
 
 `4. Напиши функцию, которая возвращает факториал числа.`
 
-const factorial = (num) => {
-  if (num < 0) 
-    return undefined;
+const getFactorial = (num) => {
+        if (typeof num !== 'number' || isNaN(num) || !Number.isInteger(num) || num < 0) {
+        return null;
+    };
 
-  if (num === 0 || num === 1) 
-    return 1;
+    if (num === 0 || num === 1) {
+        return 1;
+    };
 
-  return num * factorial(num - 1);
-}
+    return num * getFactorial(num - 1);
+};
 
-console.log(factorial(4));
+//console.log(getFactorial(4));
 
 
 `Массивы: 
@@ -79,36 +70,54 @@ console.log(factorial(4));
 1. Посчитай сумму значений массива.`
 
 const sumArr = (arr) => {
-    if(!Array.isArray(arr)) 
-        return undefined;
+    if (!Array.isArray(arr)) {
+        return null;
+    };
+    
+    if (!arr.every(num => typeof num === 'number' && !isNaN(num))) {
+        return null;
+    };
 
-    return(arr.reduce((sum, num) => {
-        return sum += num
-    }, 0))
+    return arr.reduce((sum, num) => {
+        return sum + num; 
+    }, 0);
 };
 
-console.log(sumArr([1, 2, 6]));
-console.log(sumArr(`1, 2, 3`));
+//console.log(sumArr([1, 2, 6]));
+//console.log(sumArr(`1, 2, 3`));
 
 
 `2. Выведи только чётные числа из массива.`
 
 const filterEvens = (arr) => {
-  return(arr.filter(num => num % 2 === 0).join(', ')); //по условию возвращаем не массивом
+    if (!Array.isArray(arr)) {
+        return null; 
+    };
+
+    return arr.filter(num => typeof num === 'number' && !isNaN(num) && num % 2 === 0);
 };
 
-console.log(filterEvens([1, 2, 4, 5, 6]));
-console.log(filterEvens([7, 3, 8]));
+//console.log(filterEvens([1, 2, 4, 5, 6]));
+//console.log(filterEvens([7, 3, 8]));
 
 
 `3. Отсортируй массив по убыванию.`
 
 const sortToMin = (arr) => {
-  return(arr.slice().sort((a, b) => b - a).join(', ')); 
+    if (!Array.isArray(arr)) {
+        return null;
+    };
+
+    if (!arr.every(num => typeof num === 'number' && !isNaN(num))) {
+        return null;
+    };
+
+
+    return arr.slice().sort((a, b) => a - b);
 };
 
-console.log(sortToMin([1, 2, 4, 5, 6]));
-console.log(sortToMin([7, 3, 8]));
+//console.log(sortToMin([1, 2, 4, 5, 6]));
+//console.log(sortToMin([7, 3, 8]));
 
 
 `4. Даны два массива чисел. Напишите функцию, которая создаст новый массив, содержащий только те элементы, которые встречаются в обоих массивах.`
@@ -116,33 +125,103 @@ console.log(sortToMin([7, 3, 8]));
 const arr1 = [1, 2, 3, 2, 1];
 const arr2 = [3, 2, 1, 4, 5];
 
-const combineArrays = (arr1, arr2) => {
-    let arr3 = arr1.filter(item => arr2.includes(item));//в т.ч. уникальные и повторяющиеся
+const getCommonElements = (arr1, arr2) => {
+   
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+        return null;
+    };
 
-    return arr3.sort();
-}
+    if (!arr1.every(num => typeof num === 'number' && !isNaN(num)) ||
+        !arr2.every(num => typeof num === 'number' && !isNaN(num))) {
+        return null;
+    };
 
-console.log(combineArrays(arr1, arr2));
+    const intersection = arr1.filter(item => arr2.includes(item));
+    return [...new Set(intersection)].sort((a, b) => a - b);
+};
+
+//console.log(getCommonElements([1, 2, 3, 2, 1], [3, 2, 1, 4, 5]));
 
 
 `
 5. Напишите свою реализацию метода map.
+`
+const events = [
+    {  
+        name: "Event 1", 
+        dateString: "2024-12-01" 
+    },
+    { 
+        name: "Event 2", 
+        dateString: "2024-12-15" 
+    },
+    { 
+        name: "Event 3", 
+        dateString: "2025-01-10" 
+    },
+];
+
+const eventDates = events.map(event => new Date(event.dateString));
+
+console.log(eventDates);
+
+`
 6 Напишите свою реализацию метода filter.
+`
+const users = [
+    { 
+        name: "Alice", 
+        age: 25, 
+        country: "USA" 
+    },
+    { 
+        name: "Bob", 
+        age: 17, 
+        country: "USA" 
+    },
+    { 
+        name: "Charlie", 
+        age: 30, 
+        country: "Canada" 
+    },
+    { 
+        name: "David", 
+        age: 22, 
+        country: "USA" 
+    }
+];
+
+const filteredUsers = users.filter(user => user.age > 18 && user.country === "USA");
+
+console.log(filteredUsers);
+
+`
 7. Напишите свою реализацию метода reduce.
 `
-//решила сделать три в одном, т.к. filter и reduce аналогично до этого реализовывала в задачах
 
-const implementThreeMethods = (arr2) => {
-    const arr5 = arr2.map(num => num * 2); // 6, 4, 2, 8, 10
+const cartItems = [
+    { 
+        product: "Book", 
+        price: 12.99, 
+        quantity: 2 
+    },
+    { 
+        product: "Pen", 
+        price: 1.49, 
+        quantity: 5 
+    },
+    { 
+        product: "Notebook", 
+        price: 5.99, 
+        quantity: 3 
+    }
+];
 
-    const arr6 = arr5.filter(num => num > 6) // 8, 10
+const totalCost = cartItems.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+}, 0);
 
-    const arr7 = arr6.reduce((sub, num) => sub -= num, 0) // -8 -10 = -18
-
-    return arr7;
-};
-
-console.log(implementThreeMethods(arr2));
+console.log(totalCost);
 
 
 `Объекты:
